93bd94bcae5614cd0006528a550af8b52481665b
mayankrsagar <mayankrsagar@gmail.com>
2024-11-22 17:40:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar.jsx';
+import Home from './Pages/Home/Home.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes >
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.module.css';
+
+import React from 'react';
+
+import Hero from '../../components/Hero/Hero';
+
+const Home = () => {
+  return (
+    <React.Fragment>
+        <Hero />
+    </React.Fragment>
+
+  )
+}
+
+export default Home
